79553d0138fbfb36e2105115952c33e6f96a50dc
Paranoid-AF <7659335+Paranoid-AF@users.noreply.github.com>
2020-10-24 01:24:55 +0800

    core: add electron window

---
+++ src-electron/index.ts
@@ -0,0 +1,23 @@
+import { BrowserWindow, app } from 'electron';
+import isDev from 'electron-is-dev';
+import path from 'path';
+app.on('ready', () => {
+  var win = new BrowserWindow({
+    show: false,
+    webPreferences: {
+      nodeIntegration: false,
+      nodeIntegrationInWorker: false
+    }
+  })
+  if(isDev){
+    win.loadURL(`http://localhost:3000/`)
+  }else{
+    win.loadFile(path.join(__dirname, '../build/index.html'))
+  }
+  win.on('ready-to-show', () => {
+    win.show()
+  })
+  win.on('closed', () => {
+    win.destroy()
+  })
+})
