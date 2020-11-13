6c5e8b6672fef4230e524e3556851f379a60072d
Paranoid-AF <7659335+Paranoid-AF@users.noreply.github.com>
2020-11-13 14:22:52 +0800

    add borderless mode in case of maximized

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,30 @@ import React, { Component } from 'react'
 import Navi from './Navi'
 import './App.less'
 
-class App extends Component {
+class App extends Component <{}, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      borderless: false
+    }
+  }
+
   render() {
     return (
       <div className="app-wrapper">
         <Navi />
-        <div className="container">
+        <div className={ this.state.borderless ? "container container-borderless" : "container" }>
           
         </div>
       </div>
     )
   }
+}
+
+type Props = { }
+
+type State = {
+  borderless: boolean
 }
 
 export default App
--- src/Navi/index.tsx
+++ src/Navi/index.tsx
@@ -3,14 +3,27 @@ import './index.less'
 
 import Panel from './Panel'
 
-class Navi extends Component {
+class Navi extends Component <Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      borderless: false
+    }
+  }
   render() {
     return (
-      <div className="navi">
+      <div className={ this.state.borderless ? "navi navi-borderless" : "navi" }>
         <Panel />
       </div>
     )
   }
 }
+
+type Props = { }
+
+type State = {
+  borderless: boolean
+}
+
 
 export default Navi
