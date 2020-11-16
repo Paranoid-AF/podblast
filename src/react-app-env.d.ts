667668bf06f05c80496332d9f7a3f9bec68b45ae
Paranoid-AF <7659335+Paranoid-AF@users.noreply.github.com>
2020-11-16 12:01:21 +0800

    add ipc declaration file for renderer

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="react-scripts" />
+
+declare global {
+  interface Window {
+    electron: {
+      appWindow: {
+        close: () => void,
+        maximize: () => void,
+        minimize: () => void,
+        restore: () => void
+      }
+    }
+  }
+}
+
+export interface appWindowPayload {
+  action: 'close' | 'minimize' | 'maximize' | 'restore'
+}
