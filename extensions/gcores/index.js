f903db7af30227db51846a0a23cfaa63dcaed6c6
Paranoid-AF <7659335+Paranoid-AF@users.noreply.github.com>
2020-12-11 16:00:55 +0800

    implement postForm and preForm caller.

--- extensions/gcores/index.js
+++ extensions/gcores/index.js
@@ -7,7 +7,33 @@ global.registerSource({
 });
 global.registerSource({
   id: 'gadionews',
-  name: require('./myString')
+  name: require('./myString'),
+  preForm: async function() {
+    return new Promise((resolve, reject) => {
+      resolve([
+        {
+          id: 'news-type',
+          name: '新闻类型',
+          type: 'SELECT',
+          field: [
+            {
+              description: '一起来Lu二次元',
+              value: 'lu2'
+            },
+            {
+              description: 'Gadio News Pro',
+              value: 'news-pro'
+            }
+          ]
+        }
+      ])
+    })
+  },
+  postForm: async function(data) {
+    return new Promise((resolve) => {
+      resolve('l33t' + JSON.stringify(data))
+    })
+  }
 });
 global.registerSource({
   id: 'gadiopro',
--- src-electron/extensions/child-process/listener.ts
+++ src-electron/extensions/child-process/listener.ts
@@ -1,4 +1,5 @@
-import { sources, extensions } from './'
+/* eslint-disable no-throw-literal */
+import { sources, extensions, SourceInfo } from './'
 import { resolver as resolverInit, sender as senderInit } from 'ipc-promise-invoke'
 
 const resolver = resolverInit(process)
@@ -11,6 +12,63 @@ resolver.addChannel('getExtensionList', () => {
 resolver.addChannel('getSourceList', () => {
   updateSourceList()
 })
+
+resolver.addChannel('getForm', async (payload: {id: string, provider?: string}) => {
+  let targetSource = findSource(payload.id, payload.provider)
+  if(targetSource) {
+    try {
+      return await Promise.resolve(targetSource.preForm())
+    } catch(e) {
+      let error = e
+      if(typeof e === 'object' && Object.keys(e).length === 0) {
+        error = 'preForm not found in source, or the extension has uncaught internal error.'
+      }
+      throw {
+        type: 'EXTENSION_ERROR',
+        info: error
+      } as ListenerError
+    }
+  } else {
+    throw {
+      type: 'NOT_FOUND',
+      info: 'No such source.'
+    } as ListenerError
+  }
+})
+
+resolver.addChannel('submitForm', async (payload: {id: string, provider?: string, data: Record<string, any>}) => {
+  let targetSource = findSource(payload.id, payload.provider)
+  if(targetSource) {
+    try {
+      return await Promise.resolve(targetSource.postForm(payload.data))
+    } catch(e) {
+      let error = e
+      if(typeof e === 'object' && Object.keys(e).length === 0) {
+        error = 'postForm not found in source, or the extension has uncaught internal error.'
+      }
+      throw {
+        type: 'EXTENSION_ERROR',
+        info: error
+      } as ListenerError
+    }
+  } else {
+    throw {
+      type: 'NOT_FOUND',
+      info: 'No such source.'
+    } as ListenerError
+  }
+})
+
+const findSource = (id: string, provider?: string) => {
+  let targetSource: SourceInfo | null = null
+  for(let source of sources) {
+    if(source.id === id && (!provider || provider === source.provider)) {
+      targetSource = source
+      break
+    }
+  }
+  return targetSource
+}
 
 export const updateExtensionList = () => {
   sender('extensionList', extensions)
@@ -26,4 +84,9 @@ export const updateSourceList = () => {
     }
     return properties
   }))
+}
+
+export interface ListenerError {
+  type: 'NOT_FOUND' | 'EXTENSION_ERROR',
+  info: string
 }
