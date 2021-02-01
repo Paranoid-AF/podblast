8402f5c55a073e0dc692d9a6cec86077e06dccf3
Paranoid-AF <7659335+Paranoid-AF@users.noreply.github.com>
2021-02-01 22:09:01 +0800

    load database before other operations

--- src-electron/index.ts
+++ src-electron/index.ts
@@ -5,21 +5,22 @@ import { registerEvents } from './ipc-main'
 import { initDatabase } from './data'
 import { profilePath } from './constants/path'
 
-initDatabase()
-  .catch(err => {
-    console.error(err)
-    dialog.showMessageBoxSync({
-      type: 'warning',
-      title: 'Database Error',
-      message: 'Unable to establish connection to local database, at: \n' + profilePath
-    })
-    app.quit()
-  })
-
 app.on('ready', () => {
-  initMainWindow()
-  startExtensionProcess()
-  registerEvents()
+  initDatabase()
+    .then(() => {
+      initMainWindow()
+      startExtensionProcess()
+      registerEvents()
+    })
+    .catch(err => {
+      console.error(err)
+      dialog.showMessageBoxSync({
+        type: 'warning',
+        title: 'Database Error',
+        message: 'Unable to establish connection to local database, at: \n' + profilePath
+      })
+      app.quit()
+    })
 })
 
 export default app
