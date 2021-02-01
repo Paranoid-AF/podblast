cae1225afcadf419841d787ddd624762d084764e
Paranoid-AF <7659335+Paranoid-AF@users.noreply.github.com>
2021-02-02 03:03:53 +0800

    fix typings on ipc of extension

--- src-electron/extensions/ipc.ts
+++ src-electron/extensions/ipc.ts
@@ -1,12 +1,21 @@
 import { sender as senderInit, resolver as resolverInit } from 'ipc-promise-invoke'
 import { extensionProcess } from './'
 
-export const resolver: Array<any> = []
-export const sender: Array<any> = []
+export const resolver: ReturnType<typeof resolverInit> = [
+  (channel: string, action: (...args: any) => any) => {},
+  (channel: string) => {},
+  () => {}
+]
+export const sender: ReturnType<typeof senderInit> = [
+  async (channel: string, ...payload: any) => {},
+  () => {}
+]
 
 export const buildIpcBridge = () => {
   if(extensionProcess){
+    resolver.splice(0, 3)
     resolver.push.apply(resolver, resolverInit(extensionProcess))
+    sender.splice(0, 2)
     sender.push.apply(sender, senderInit(extensionProcess))
   }
 }
