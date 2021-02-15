5dc697f235abbe0e8bfb476be4c8e7dbeab61c43
Paranoid-AF <7659335+Paranoid-AF@users.noreply.github.com>
2021-02-15 17:08:33 +0800

    minor tweaks to navi item style.

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -7,20 +7,17 @@ function Home(props: DispatchProps) {
   return (
     <PageBase title="Podchat">
       <React.Fragment>
-        Welcome back home!
-        <button onClick={() => { window.electron.invoke('player', { action: 'togglePlayerWindow', payload: true }) }}>Show PIP</button>
-        <button onClick={() => { window.electron.invoke('player', { action: 'togglePlayerWindow', payload: false }) }}>Hide PIP</button>
         <p>
         <button
           onClick={
             () => {
               props.setUpUserAgent(`Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0 Safari/605.1.15`)
-              props.setUpProxy('gcores')
+              props.setUpProxy('rss')
                 .then(() => {
                   props.startPlaying({
                     url: 'https://www.youtube.com/watch?v=JQIcUBd-5fk',
                     nowPlayingPage: 'https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik',
-                    coverArt: 'https://assets.fireside.fm/file/fireside-images/podcasts/images/b/bcdeb9eb-7a8c-4a76-a424-1023c5d280b0/cover_small.jpg?v=3',
+                    coverArt: 'https://i.ytimg.com/vi/JQIcUBd-5fk/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCDDwOQ1-viPim0ewEk0lf5RbxpZw',
                     title: 'Pessimism is *literally* for losers.',
                     channel: 'Luke Smith'
                   })
@@ -28,7 +25,26 @@ function Home(props: DispatchProps) {
             }
           }
         >
-          Rickroll
+          YouTube Test
+        </button>
+        <button
+          onClick={
+            () => {
+              props.setUpUserAgent(`Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0 Safari/605.1.15`)
+              props.setUpProxy('gcores')
+                .then(() => {
+                  props.startPlaying({
+                    url: 'https://alioss.gcores.com/uploads/audio/e082fd39-cf09-4139-9241-fbc34fd24787.mp3',
+                    nowPlayingPage: 'https://www.gcores.com/radios/111117',
+                    coverArt: 'https://image.gcores.com/d07f4653-7c59-4776-a814-9a93d4c96c68.jpg?x-oss-process=image/resize,limit_1,m_fill,w_280,h_280/quality,q_90',
+                    title: '《玲音》的诞生与创作《玲音》的人',
+                    channel: '特别二次元'
+                  })
+                })
+            }
+          }
+        >
+          Audio Test
         </button>
         </p>
       </React.Fragment>
