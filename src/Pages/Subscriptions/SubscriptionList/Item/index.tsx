4eb4f6a8640e8fe12a1061a2684a57c7ed91528e
Paranoid-AF <7659335+Paranoid-AF@users.noreply.github.com>
2021-05-02 23:41:55 +0800

    add context menu for subscription item

--- src/Pages/Subscriptions/SubscriptionList/Item/index.tsx
+++ src/Pages/Subscriptions/SubscriptionList/Item/index.tsx
@@ -3,8 +3,53 @@ import './index.less'
 import type {
   Subscription,
 } from '../../../../../src-electron/data/entity/Subscription'
+import { Menu, Dropdown, Modal } from 'antd';
+
+enum MenuActions {
+  OPEN = 'OPEN',
+  PIN_TO_SIDEBAR = 'PIN_TO_SIDEBAR',
+  REMOVE = 'REMOVE'
+}
 
 class SubscriptionListItem extends React.PureComponent<Subscription> {
+  handleRemoveItem = () => {
+    console.log('Removing sub: '+this.props.uuid)
+  }
+
+  handleMenuClick = (event: { key: MenuActions }) => {
+    switch(event.key) {
+      case MenuActions.OPEN: {
+        console.log('Opening...')
+        break
+      }
+      case MenuActions.PIN_TO_SIDEBAR: {
+        console.log('Pinned to sidebar...')
+        break
+      }
+      case MenuActions.REMOVE: {
+        Modal.confirm({
+          title: 'Remove Subscription',
+          content: (
+            <span>
+              <p>You're about to remove the following subscription.</p>
+              <p>
+                Name: {this.props.title} <br/>
+                Source: {this.props.source}
+              </p>
+              <p>This operation may not be reverted.</p>
+            </span>
+          ),
+          okText: 'Remove',
+          onOk: this.handleRemoveItem,
+          okButtonProps: {
+            danger: true
+          }
+        })
+        break
+      }
+    }
+  }
+
   renderCover() {
     const style: React.CSSProperties = {}
     if(this.props.cover_color) {
@@ -22,14 +67,25 @@ class SubscriptionListItem extends React.PureComponent<Subscription> {
     )
   }
 
+  menu = (
+    <Menu onClick={this.handleMenuClick as any}>
+      <Menu.Item key={MenuActions.OPEN}>Open</Menu.Item>
+      <Menu.Item key={MenuActions.PIN_TO_SIDEBAR}>Pin to Sidebar</Menu.Item>
+      <Menu.Divider />
+      <Menu.Item key={MenuActions.REMOVE} danger>Remove Subscription</Menu.Item>
+    </Menu>
+  )
+
   render() {
     return (
-      <div className="sub-item">
-        {this.renderCover()}
-        <span className="title">{this.props.title}</span>
-        <p></p>
-        <span className="source">{this.props.source}</span>
-      </div>
+      <Dropdown overlay={this.menu} trigger={['contextMenu']}>
+        <div className="sub-item">
+          {this.renderCover()}
+          <span className="title">{this.props.title}</span>
+          <p></p>
+          <span className="source">{this.props.source}</span>
+        </div>
+      </Dropdown>
     )
   } 
 }
